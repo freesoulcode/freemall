b7c4760f3ef7133a30f69dab1f3de14fd2ecbdb8
freesoulcode <75832952+freesoul17@users.noreply.github.com>
2026-04-25 01:19:16 +0800

    feat(project): 添加商家后台BFF服务模块

---
+++ freemall-bff-seller/src/main/java/io/github/freesoulcode/bff/seller/FreeMallBffSellerApplication.java
@@ -0,0 +1,7 @@
+package io.github.freesoulcode.bff.seller;
+@SpringBootApplication
+public class FreeMallBffSellerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMallBffAdminApplication.class, args);
+    }
+}
